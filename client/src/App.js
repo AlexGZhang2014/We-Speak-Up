df64076d20eb679599974322becec5a88ca5db14
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-10-06 18:18:35 -0400

    Implement CSS

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <header className="App-header">
+          <h1>We Speak Up</h1>
+          <p>This is a discussion board for survivors of abuse to discuss their thoughts, feelings, experiences, and opinions in a safe, non-judgmental environment. Feel free to share anything that you are comfortable with.</p>
+        </header>
         <PostContainer />
       </div>
     );
