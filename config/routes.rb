e70742c071c094085fad2db76d99f9df05e10624
AlexGZhang2014 <alexgzhang92@gmail.com>
2018-10-07 01:15:25 -0400

    Finish Heroku setup

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
       resources :posts
     end
   end
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
